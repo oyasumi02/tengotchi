7043bd1d44e7b46da82486ec12ef5db7b1e6f2bd
oyasumi02 <jaguilar1999@live.com>
2025-09-06 09:49:07 -0500

    Add screen_type in if cases

--- include/input/input.hpp
+++ include/input/input.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
+#include "../state/state.hpp"
 
 namespace Input {
-    void ScreenCase(std::string &input);
+    void ScreenCase(std::string &input, State::ScreenType &screen_type);
 }
--- src/input/input.cpp
+++ src/input/input.cpp
@@ -1,20 +1,21 @@
 #include "../../include/input/input.hpp"
 #include "../../include/sumistd/sumistd.hpp"
+#include "../../include/state/state.hpp"
 
-void Input::ScreenCase(std::string &input) {
+void Input::ScreenCase(std::string &input, State::ScreenType &screen_type) {
     input = sumistd::ToLower(input);
     if (input == "feed") {
-
+        screen_type = State::ScreenType::FEED;
     } else if (input == "lights" || input == "light") {
-
+        screen_type = State::ScreenType::LIGHTS;
     } else if (input == "play") {
-
+        screen_type = State::ScreenType::PLAY;
     } else if (input == "discipline") {
-
+        screen_type = State::ScreenType::DISCIPLINE;
     } else if (input == "sick") {
-
+        screen_type = State::ScreenType::SICK;
     } else if (input == "stats") {
-
+        screen_type = State::ScreenType::STATS;
     } else {
         sumistd::Flush();
         std::cout << input << " is not a valid option!" << std::endl;
